ec60f0a8341f53e0293870761c535c49c95f5d74
caru-marques <caru.marques@gmail.com>
2022-03-25 10:57:14 -0400

    Endpoint para recuperar pedido ativo pelo ID do Driver

---
+++ apientregador/src/main/java/com/kintsugi/apientregador/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.kintsugi.apientregador.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kintsugi.apientregador.dao.OrderDAO;
+import com.kintsugi.apientregador.model.Order;
+
+@RestController
+public class OrderController {
+	
+	@Autowired
+	private OrderDAO dao;
+
+	@GetMapping("/orders/driver/{idDriver}")
+	public List<Order> recuperarPedidoIdDriver(@PathVariable Integer idDriver) {
+		List<Order> lista; 
+		lista = (List<Order>)dao.findAllByIdDriverAndStatusInTransit(idDriver);
+		return lista;
+	}
+	
+}
